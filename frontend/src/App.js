c7b4759c49446ed2280932be16f15642da2a3d81
dcarroll9398 <dcarroll9398@yahoo.com>
2022-06-23 22:24:05 -0500

    Video-03

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">
+          <strong>amazona</strong>
+        </a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
